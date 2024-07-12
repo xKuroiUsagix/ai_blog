c4c28f1b1c30a4904002c037ac4971aae5edb4cc
kodo <kodokawaiosu@gmail.com>
2024-07-12 22:30:47 +0300

    create user api, update user model

--- user/api.py
+++ user/api.py
@@ -1,19 +1,33 @@
 from ninja_extra import api_controller, route
 from ninja_jwt.authentication import JWTAuth
+from ninja.errors import HttpError
 
 from .models import User
 from .schemas import UserIn, UserOut
+from .constants import NEGATIVE_AUTO_POST_REPLY_ERROR
 
 
 @api_controller
 class UserController:
-    @route.get('/', response={200: UserOut}, auth=JWTAuth())
+    @route.get('/', auth=JWTAuth(), response={200: UserOut})
     def retrieve_user(self, request):
         return request.user
 
     @route.post('create/', response={201: UserOut})
     def create_user(self, request, data: UserIn):
+        if data.get('auto_post_reply') < 0:
+            raise HttpError(400, NEGATIVE_AUTO_POST_REPLY_ERROR)
+
         user = User(username=data.username)
         user.set_password(data.password)
         user.save()
         return user
+
+    @route.patch('update-auto-reply/', auth=JWTAuth())
+    def update_auto_reply(self, request, auto_post_reply: int):
+        if auto_post_reply < 0:
+            raise HttpError(400, NEGATIVE_AUTO_POST_REPLY_ERROR)
+
+        request.user.auto_post_reply = auto_post_reply
+        request.user.save()
+        return request.user
--- user/schemas.py
+++ user/schemas.py
@@ -1,11 +1,13 @@
+from typing import Optional
 from ninja import Schema
 
 
 class UserIn(Schema):
     username: str
     password: str
-
+    auto_post_reply: Optional[int]
 
 class UserOut(Schema):
     id: int
     username: str
+    auto_post_reply: Optional[int]
