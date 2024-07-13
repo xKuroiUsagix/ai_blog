4c060167a098e749ce1bf2c1830b7704ce081441
kodo <kodokawaiosu@gmail.com>
2024-07-13 16:26:22 +0300

    create ai verification and responses

--- blog/helpers.py
+++ blog/helpers.py
@@ -1,7 +1,25 @@
+from google.generativeai.types import HarmCategory, HarmBlockThreshold, BlockedReason
+
+from ai_blog.gemini import ai_model
+
+from .constants import MAX_COMMENT_LENGTH
+
+
 def get_ai_response(content):
-    # Not Implemented Yet
-    return 'Temporary mock response'
+    limit = int(MAX_COMMENT_LENGTH / 2)
+    return ai_model.generate_content(f'Reply to this comment using less than {limit} symbols: {content}')
 
 def ai_verify_safety(content):
-    # Not Implemented Yet
+    response = ai_model.generate_content(
+        f'Is this text save to public: {content}',
+        safety_settings= {
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+        }
+    )
+
+    if response.candidates[0].finish_reason == BlockedReason.SAFETY:
+        return False
     return True
