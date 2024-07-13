40b6b5bfd94ade28b88b339248a82581e8b1c39d
kodo <kodokawaiosu@gmail.com>
2024-07-14 02:54:22 +0300

    better design for user schemas

--- user/constants.py
+++
@@ -1,1 +0,0 @@
-NEGATIVE_AUTO_POST_REPLY_ERROR = 'auto_post_reply value cannot be negative.'
--- user/schemas.py
+++ user/schemas.py
@@ -1,11 +1,12 @@
 from typing import Optional
+from pydantic import NonNegativeInt
 from ninja import Schema
 
 
 class UserInputSchema(Schema):
     username: str
     password: str
-    auto_post_reply: Optional[int]
+    auto_post_reply: Optional[NonNegativeInt]
 
 class UserOutputSchema(Schema):
     id: int
