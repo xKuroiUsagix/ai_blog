b0b8d7762b61ab9501fb8aefc76d35ac630491cb
kodo <kodokawaiosu@gmail.com>
2024-07-14 02:54:22 +0300

    better design for user schemas

--- user/api.py
+++ user/api.py
@@ -1,11 +1,9 @@
 from ninja_extra import api_controller, route
-from ninja_extra import Router
 from ninja_jwt.authentication import JWTAuth
-from ninja.errors import HttpError
+from pydantic import NonNegativeInt
 
 from .models import User
 from .schemas import UserInputSchema, UserOutputSchema
-from .constants import NEGATIVE_AUTO_POST_REPLY_ERROR
 
 
 @api_controller('/user')
@@ -16,19 +14,16 @@ class UserController:
 
     @route.post('/create', response={201: UserOutputSchema})
     def create_user(self, request, data: UserInputSchema):
-        if data.get('auto_post_reply') < 0:
-            raise HttpError(400, NEGATIVE_AUTO_POST_REPLY_ERROR)
-
-        user = User(username=data.username)
+        user = User(
+            username = data.username,
+            auto_post_reply = data.auto_post_reply
+        )
         user.set_password(data.password)
         user.save()
         return user
 
     @route.patch('/update-auto-reply', auth=JWTAuth(), response={200: UserOutputSchema})
-    def update_auto_reply(self, request, auto_post_reply: int):
-        if auto_post_reply < 0:
-            raise HttpError(400, NEGATIVE_AUTO_POST_REPLY_ERROR)
-
+    def update_auto_reply(self, request, auto_post_reply: NonNegativeInt):
         request.user.auto_post_reply = auto_post_reply
         request.user.save()
         return request.user
