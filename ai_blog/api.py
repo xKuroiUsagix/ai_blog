9af8b9613b8638f7ae959e07e1c9e15d050ccbb1
kodo <kodokawaiosu@gmail.com>
2024-07-14 12:14:45 +0300

    refactor api controllers structure

--- ai_blog/api.py
+++ ai_blog/api.py
@@ -2,13 +2,12 @@ from ninja_jwt.controller import NinjaJWTDefaultController
 from ninja_extra import NinjaExtraAPI
 
 from user.api import UserController
-from blog.api import PostController, CommentController
+from blog.api import BlogController
 
 
 api = NinjaExtraAPI()
 api.register_controllers(
     NinjaJWTDefaultController,
     UserController,
-    PostController,
-    CommentController
+    BlogController
 )
