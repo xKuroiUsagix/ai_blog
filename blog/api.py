794b8dd7ff94301c6ad4faa04a6aaf0ae6427306
kodo <kodokawaiosu@gmail.com>
2024-07-14 14:22:17 +0300

    finish api for posts and comments managment

--- blog/api.py
+++ blog/api.py
@@ -4,18 +4,25 @@ from ninja_extra import api_controller, route, permissions
 from ninja_jwt.authentication import JWTAuth
 from ninja.errors import HttpError
 from django.shortcuts import get_object_or_404
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from django.db.models import Q
+from rest_framework.status import (
+    HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT,
+    HTTP_403_FORBIDDEN
+)
 
 from .models import Post, Comment, CommentResponse
 from .schemas import (
     PostInputSchema, 
-    PostOutputSchema, 
+    PostOutputSchema,
+    PostUpdateSchema,
     CommentOutputSchema, 
     CommentInputSchema,
-    CommentResponseSchema
 )
 from .helpers import ai_verify_safety
-from .constants import HARMFUL_CONTENT_ERROR
+from .constants import (
+    HARMFUL_CONTENT_ERROR, BLOCKED_COMMENT_ERROR, WRONG_USER_POST_ERROR, 
+    WRONG_USER_COMMENT_ERROR, POST_UPDATE_NO_FIELDS_ERROR
+)
 
 
 @api_controller('/blog', auth=JWTAuth(), permissions=[permissions.IsAuthenticated])
@@ -24,7 +31,7 @@ class BlogController:
     def retrieve_post(self, request, post_id: int):
         return get_object_or_404(Post, id=post_id)
 
-    @route.get('/post/list', response={HTTP_200_OK: List[PostOutputSchema]})
+    @route.get('/post-list', response={HTTP_200_OK: List[PostOutputSchema]})
     def retrieve_all_posts(self, request):
         return Post.objects.all()
 
@@ -35,9 +42,9 @@ class BlogController:
     @route.get('/post/{post_id}/comments', response={HTTP_200_OK: List[CommentOutputSchema]})
     def retrieve_post_comments(self, request, post_id: int):
         post = get_object_or_404(Post, id=post_id)
-        return post.comment.all().exclude(is_response=True).order_by('-created_at')
+        return post.comment.all().exclude(Q(is_response=True) | Q(is_blocked=True)).order_by('-created_at')
 
-    @route.post('/post/create', response={HTTP_201_CREATED: PostOutputSchema})
+    @route.post('/create-post', response={HTTP_201_CREATED: PostOutputSchema})
     def create_post(self, request, data: PostInputSchema):
         if not ai_verify_safety(data.content):
             raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
@@ -49,42 +56,78 @@ class BlogController:
         )
 
     @route.patch('/post/{post_id}/update', response={HTTP_200_OK: PostOutputSchema})
-    def update_post(self, request, content: str, post_id: int):
-        if not ai_verify_safety(content):
-            raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
+    def update_post(self, request, post_id: int, data: PostUpdateSchema):
+        if data.title is None and data.content is None:
+            raise HttpError(HTTP_400_BAD_REQUEST, POST_UPDATE_NO_FIELDS_ERROR)
+
+        if data.content:
+            if not ai_verify_safety(data.content):
+                raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
 
         post = get_object_or_404(Post, id=post_id)
-        post.content = content
+        
+        if data.title:
+            post.title = data.title
+        if data.content:
+            post.content = data.content
+
         post.save()
 
         return post
 
+    @route.delete('/post/{post_id}/delete')
+    def delete_post(self, request, post_id: int):
+        post = get_object_or_404(Post, id=post_id)
+
+        if post.user != request.user:
+            return HttpError(HTTP_403_FORBIDDEN, WRONG_USER_POST_ERROR)
+
+        post.delete()
+        return HTTP_204_NO_CONTENT
+
     @route.get('/comment/{comment_id}', response={HTTP_200_OK: CommentOutputSchema})
-    def retrieve_comment(self, request, comment_id):
-        return get_object_or_404(Comment, id=comment_id)
+    def retrieve_comment(self, request, comment_id: int):
+        comment = get_object_or_404(Comment, id=comment_id)
+        
+        if comment.is_blocked:
+            raise HttpError(HTTP_400_BAD_REQUEST, BLOCKED_COMMENT_ERROR)
+        return comment
 
-    @route.post('/post/comment/create', response={HTTP_201_CREATED: CommentOutputSchema})
-    def create_comment(self, request, data: CommentInputSchema):
-        if not ai_verify_safety(data.content):
+    @route.post('/post/{post_id}/create-comment', response={HTTP_201_CREATED: CommentOutputSchema})
+    def create_comment(self, request, post_id: int, data: CommentInputSchema):
+        is_blocked = not ai_verify_safety(data.content)
+        comment = self._create_comment(request.user, data, post_id=post_id, is_blocked=is_blocked)
+
+        if is_blocked:
             raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
+        return comment
 
-        return self._create_comment(request.user, data)
-
-    @route.post('/comment/reply', response={HTTP_201_CREATED: CommentOutputSchema})
-    def reply_to_comment(self, request, data: CommentResponseSchema):
-        if not ai_verify_safety(data.content):
-            raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
-
-        comment = get_object_or_404(Comment, id=data.reply_to)
-        response = self._create_comment(request.user, data, is_response=True)
+    @route.post('post/{post_id}/comment/{comment_id}/reply', response={HTTP_201_CREATED: CommentOutputSchema})
+    def reply_to_comment(self, request, post_id: int, comment_id: int, data: CommentInputSchema):
+        is_blocked = not ai_verify_safety(data.content)
+        comment = get_object_or_404(Comment, id=comment_id)
+        response = self._create_comment(request.user, data, post_id=post_id, is_response=True, is_blocked=is_blocked)
         CommentResponse.objects.create(comment=comment, response=response)
 
+        if is_blocked:
+            raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
         return response
 
-    def _create_comment(self, user, data: CommentInputSchema, **kwargs):
+    @route.delete('post/{post_id}/comment/{comment_id}')
+    def delete_comment(self, request, post_id: int, comment_id: int):
+        comment = get_object_or_404(Comment, id=comment_id)
+        post = get_object_or_404(Post, id=post_id)
+
+        if comment.user != request.user and post.user != request.user:
+            raise HttpError(HTTP_403_FORBIDDEN, WRONG_USER_COMMENT_ERROR)
+
+        comment.delete()
+        return HTTP_204_NO_CONTENT
+
+    def _create_comment(self, user, data: CommentInputSchema, post_id, **kwargs):
         return Comment.objects.create(
             user = user,
-            post = get_object_or_404(Post, id=data.post),
+            post = get_object_or_404(Post, id=post_id),
             content = data.content,
             **kwargs
         )
--- blog/constants.py
+++ blog/constants.py
@@ -1,3 +1,9 @@
 MAX_COMMENT_LENGTH = 512
 
-HARMFUL_CONTENT_ERROR = 'Provided content was considered as harmful.'
+HARMFUL_CONTENT_ERROR = 'Provided content was considered as harmful and was blocked.'
+BLOCKED_COMMENT_ERROR = 'The comment you are trying to recieve was blocked due to safery reasons.'
+
+WRONG_USER_POST_ERROR = 'You are not the author of the post.'
+WRONG_USER_COMMENT_ERROR = 'You are not the author of the post nor the author of the comment.'
+
+POST_UPDATE_NO_FIELDS_ERROR = 'At least one field must be provided.'
