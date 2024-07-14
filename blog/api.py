f23ac7942e3d341218a9c4623324f07f8e5c9349
kodo <kodokawaiosu@gmail.com>
2024-07-14 12:14:45 +0300

    refactor api controllers structure

--- blog/api.py
+++ blog/api.py
@@ -4,6 +4,7 @@ from ninja_extra import api_controller, route, permissions
 from ninja_jwt.authentication import JWTAuth
 from ninja.errors import HttpError
 from django.shortcuts import get_object_or_404
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from .models import Post, Comment, CommentResponse
 from .schemas import (
@@ -17,29 +18,29 @@ from .helpers import ai_verify_safety
 from .constants import HARMFUL_CONTENT_ERROR
 
 
-@api_controller('/blog/post', auth=JWTAuth(), permissions=[permissions.IsAuthenticated])
-class PostController:
-    @route.get('/', response={200: PostOutputSchema})
+@api_controller('/blog', auth=JWTAuth(), permissions=[permissions.IsAuthenticated])
+class BlogController:
+    @route.get('/post/{post_id}', response={HTTP_200_OK: PostOutputSchema})
     def retrieve_post(self, request, post_id: int):
         return get_object_or_404(Post, id=post_id)
 
-    @route.get('/list', response={200: List[PostOutputSchema]})
-    def retrieve_posts_list(self, request):
+    @route.get('/post/list', response={HTTP_200_OK: List[PostOutputSchema]})
+    def retrieve_all_posts(self, request):
         return Post.objects.all()
 
-    @route.get('/user/list', response={200: List[PostOutputSchema]})
+    @route.get('/user/{username}/posts', response={HTTP_200_OK: List[PostOutputSchema]})
     def retrieve_user_posts(self, request, username: str):
         return Post.objects.filter(user__username=username).order_by('-created_at')
     
-    @route.get('/comments', response={200: List[CommentOutputSchema]})
+    @route.get('/post/{post_id}/comments', response={HTTP_200_OK: List[CommentOutputSchema]})
     def retrieve_post_comments(self, request, post_id: int):
         post = get_object_or_404(Post, id=post_id)
         return post.comment.all().exclude(is_response=True).order_by('-created_at')
 
-    @route.post('/create', response={201: PostOutputSchema})
+    @route.post('/post/create', response={HTTP_201_CREATED: PostOutputSchema})
     def create_post(self, request, data: PostInputSchema):
         if not ai_verify_safety(data.content):
-            raise HttpError(400, HARMFUL_CONTENT_ERROR)
+            raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
 
         return Post.objects.create(
             user = request.user,
@@ -47,10 +48,10 @@ class PostController:
             content = data.content
         )
 
-    @route.patch('/update-content', response={200: PostOutputSchema})
+    @route.patch('/post/{post_id}/update', response={HTTP_200_OK: PostOutputSchema})
     def update_post(self, request, content: str, post_id: int):
         if not ai_verify_safety(content):
-            raise HttpError(400, HARMFUL_CONTENT_ERROR)
+            raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
 
         post = get_object_or_404(Post, id=post_id)
         post.content = content
@@ -58,24 +59,21 @@ class PostController:
 
         return post
 
-
-@api_controller('/blog/comment', auth=JWTAuth(), permissions=[permissions.IsAuthenticated])
-class CommentController:
-    @route.get('/', response={200: CommentOutputSchema})
+    @route.get('/comment/{comment_id}', response={HTTP_200_OK: CommentOutputSchema})
     def retrieve_comment(self, request, comment_id):
         return get_object_or_404(Comment, id=comment_id)
 
-    @route.post('/create', response={201: CommentOutputSchema})
+    @route.post('/post/comment/create', response={HTTP_201_CREATED: CommentOutputSchema})
     def create_comment(self, request, data: CommentInputSchema):
         if not ai_verify_safety(data.content):
-            raise HttpError(400, HARMFUL_CONTENT_ERROR)
+            raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
 
         return self._create_comment(request.user, data)
 
-    @route.post('/reply', response={201: CommentOutputSchema})
+    @route.post('/comment/reply', response={HTTP_201_CREATED: CommentOutputSchema})
     def reply_to_comment(self, request, data: CommentResponseSchema):
         if not ai_verify_safety(data.content):
-            raise HttpError(400, HARMFUL_CONTENT_ERROR)
+            raise HttpError(HTTP_400_BAD_REQUEST, HARMFUL_CONTENT_ERROR)
 
         comment = get_object_or_404(Comment, id=data.reply_to)
         response = self._create_comment(request.user, data, is_response=True)
